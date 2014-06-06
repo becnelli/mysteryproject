7bada8b17114da9716d3a8ac2c5be6ac9620918b
Stefan Kendall <stefankendall@gmail.com>
2014-06-06 18:23:08 -0400

    calculating bang value

--- js/App.js
+++ js/App.js
@@ -23,11 +23,89 @@ Ext.define('MyApp.App', {
     
     _boardToggled: function(sender, attribute){
         this.remove(this.board);
+        this.remove(this.grid);
         
         if(attribute != 'Ranking') {
             this._addBoard(attribute);
         }
+        else {
+            this._addRankingGrid();
+        }
     },
+    
+    _addRankingGrid: function() {
+        Ext.create('Rally.data.wsapi.Store', {
+            model: 'userstory',
+            autoLoad: true,
+            listeners: {
+                load: this._onGridDataLoaded,
+                scope: this
+            },
+            filters: [
+                {
+                    property: 'PlanEstimate',
+                    operator: '!=',
+                    value: null
+                },
+                {
+                    property: 'c_BusinessValue',
+                    operator: '!=',
+                    value: null
+                },
+                {
+                    property: 'DirectChildrenCount',
+                    operator: '=',
+                    value: '0'
+                }
+            ],
+            fetch: true
+        });
+    },
+    
+    _onGridDataLoaded: function(store, data) {
+        var records = _.map(data, function(record) {
+            return Ext.apply({
+                Bang: record.get('c_BusinessValue') / record.get('PlanEstimate')
+            }, record.getData());
+        });
+    
+        this.grid = this.add({
+            xtype: 'rallygrid',
+            showPagingToolbar: false,
+            showRowActionsColumn: false,
+            editable: false,
+            store: Ext.create('Rally.data.custom.Store', {
+                data: records
+            }),
+            columnCfgs: [
+                {
+                    xtype: 'templatecolumn',
+                    text: 'ID',
+                    dataIndex: 'FormattedID',
+                    width: 100,
+                    tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')
+                },
+                {
+                    text: 'Name',
+                    dataIndex: 'Name',
+                    flex: 1
+                },
+                {
+                    text: 'Plan Estimate',
+                    dataIndex: 'Plan Estimate'
+                },
+                {
+                    text: 'Business Value',
+                    dataIndex: 'Business Value'
+                },
+                {
+                    text: 'Bang',
+                    dataIndex: 'Bang'
+                }
+            ]
+        });
+    },
+    
     
     _addBoard: function(attribute) {
         var columns = [
@@ -68,12 +146,12 @@ Ext.define('MyApp.App', {
                 filters: [
                     {
                         property: 'Iteration',
-                        operation: '=',
+                        operator: '=',
                         value: ''
                     },
                     {
                         property: 'DirectChildrenCount',
-                        operation: '=',
+                        operator: '=',
                         value: '0'
                     }
                 ]
@@ -82,5 +160,4 @@ Ext.define('MyApp.App', {
             columns: columns
         });
     }
-    
 });
